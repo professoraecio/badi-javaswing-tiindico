48c7d066ab13bafde7e77e4c21d13d35dcb89579
Professor Aécio <professoraecio@gmail.com>
2023-06-19 23:13:12 -0300

    Criar tela vazia celular
    Criar tela vazia celular

---
+++ Tiindico/src/main/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import view.WelcomeViewController;
+
+/**
+ *
+ * @author mastermac
+ */
+public class Principal {
+    public static void main(String[] args) {
+        WelcomeViewController welcomeViewController = new WelcomeViewController();
+        welcomeViewController.setVisible(true);
+        welcomeViewController.setLocationRelativeTo(null);
+    }
+    
+}
